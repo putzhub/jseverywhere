814036d19035c64bbec6f5a48ec32236abce511d
Pierce Putz <git@putz.space>
2023-06-13 17:13:44 -0500

    finish chapter 5

---
+++ src/db.js
@@ -0,0 +1,31 @@
+//const { error } = require('console');
+const mongoose = require('mongoose');
+
+module.exports = {
+    connect: DB_HOST => {
+        //debug
+        mongoose.set('debug', true);
+        //Use the Mongo driver's updated URL string parser
+        mongoose.set('useNewUrlParser', true);
+        //Use findOneAndUpdate() in place of findAndModify()
+        mongoose.set('useFindAndModify', false);
+        //Use createIndex() in place of ensureIndex()
+        mongoose.set('useCreateIndex', true);
+        //Use the new server discovery and monitoring engine
+        mongoose.set('useUnifiedTopology', true);
+        //connect to the DB
+        mongoose.connect(DB_HOST);
+        //Log an error if we fail to connect
+        mongoose.connection.on('error', err => {
+            console.error(err);
+            console.log(
+                'MongoDB Connection error. Please make sure MongoDB is running.'
+            );
+            process.exit();
+        });
+    },
+
+    close: () => {
+        mongoose.connection.close();
+    }
+};
